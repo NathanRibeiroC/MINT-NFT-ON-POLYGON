0497cd583e88830b25d82da419e8b508769d5962
Nathan Ribeiro <nathanazevedo@gec.inatel.br>
2023-02-09 21:27:33 -0300

    card about tab alignment

--- client/src/components/HomeComponents/ProfileCard/AboutTab.tsx
+++ client/src/components/HomeComponents/ProfileCard/AboutTab.tsx
@@ -4,11 +4,12 @@ import { motion, AnimatePresence } from "framer-motion";
 import profilePicture from "../../../../public/profile img-1.jpg";
 import githubIcon from "../../../../public/iconmonstr-github-1.svg";
 import linkedinIcon from "../../../../public/iconmonstr-linkedin-4.svg";
+import GithubIcon from "../../../assets/GithubIcon";
 
 function AboutTab() {
   return (
     <div className="about-tab">
-        <img src={profilePicture}/>
+        <img src={profilePicture} className="about-profile-picture"/>
         <h1>William Rocheald</h1>
         <h2>UI DEVELOPER</h2>
         <div className="main-text">
@@ -18,8 +19,9 @@ function AboutTab() {
             </p>
         </div>
         <div className="contact-card">
-            <img src={githubIcon}/>
-            <img src={linkedinIcon}/>
+            <GithubIcon fill="blue"/>
+            <img src={githubIcon} className="github-icon"/>
+            <img src={linkedinIcon} className="linkedin-icon"/>
         </div>
     </div>
   );
