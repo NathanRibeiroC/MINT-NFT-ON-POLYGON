b9556bf84cffc9ad556275c4d5c8965a4d4b2e69
Nathan Ribeiro <nathanazevedo@gec.inatel.br>
2023-02-23 19:54:25 -0300

    feat(CUSTOM HOOK): :hammer: Created custom hook for delaying animation pop up
    Custom hook that takes as a parameter a value in millisecondas, with the use of a useEffect it sets a timeout wheter the component should appear or not, this is very useful for waiting animations render before showing them

--- client/src/components/HomeComponents/ProfileCard/AnimatedWavesComponents.tsx
+++ client/src/components/HomeComponents/ProfileCard/AnimatedWavesComponents.tsx
@@ -3,6 +3,7 @@ import { ReactComponent as Wave2 } from '../../../../public/wave2.svg';
 import { ReactComponent as Wave3 } from '../../../../public/wave3.svg';
 import { ReactComponent as Wave4 } from '../../../../public/wave4.svg';
 import { Suspense, useEffect, useState } from 'react';
+import useAnimationDelay from '../../../hooks/useAnimationDelay';
 
 interface AnimatedWavesComponentsProps{
   classNameProp: string,
@@ -11,19 +12,9 @@ interface AnimatedWavesComponentsProps{
 const MILLISECONDS = 2000;
 
 function AnimatedWavesComponents(props: AnimatedWavesComponentsProps) {
-  const [initialComponentVisibility, setInitialComponentVisibility] = useState<any>(undefined);
-  let style: { visibility?: string } = {};
-  useEffect(
-    () => {
-      let timer1 = setTimeout(() => setInitialComponentVisibility(true), MILLISECONDS);
-      return () => {
-        clearTimeout(timer1);
-      };
-    },
-    []
-  );
-    return (
-      <div style={{visibility : initialComponentVisibility ? 'visible' : 'hidden'}}>
+
+  return (
+      <div style={{visibility : useAnimationDelay(MILLISECONDS) ? 'visible' : 'hidden'}}>
         <Wave1 className={props.classNameProp}/>
         <Wave2 className={props.classNameProp}/>
         <Wave3 className={props.classNameProp}/>
--- client/src/hooks/useAnimationDelay.ts
+++ client/src/hooks/useAnimationDelay.ts
@@ -1,4 +1,17 @@
+import { Suspense, useEffect, useState, Dispatch, SetStateAction } from 'react';
 
-export default function useAnimationDelay(milliseconds:number){
-    return new Promise(resolve => setTimeout(resolve, milliseconds))
+export default function useAnimationDelay(milliseconds: number){
+    const [initialState, setInitialState] = useState<boolean>(false);
+
+    useEffect(
+        () => {
+          let timer1 = setTimeout(() =>  setInitialState(true), milliseconds);
+          return () => {
+            clearTimeout(timer1);
+          };
+        },
+        []
+    );
+
+    return initialState;
 }
