ef8e84cf9492ea259395ab2c8aa638e898822946
Nathan Linux Machine <nathanazevedo@gec.inatel.br>
2023-03-23 00:04:37 -0700

    style(UI): :art: ADJUSTING GRID ON TEH FORM

--- client/src/components/HomeComponents/ProfileCard/SendMessage.tsx
+++ client/src/components/HomeComponents/ProfileCard/SendMessage.tsx
@@ -5,6 +5,7 @@ import GeneralFormInput from "../../GeneralComponents/GeneralFormInput";
 import GeneralTextArea from "../../GeneralComponents/GeneralTextArea";
 import useSubmit from "../../../utils/useSubmit";
 import ReturnButton from "../../GeneralComponents/ReturnButton";
+import "./_snd_msg_form.scss"
 
 function SendMessageCard() {
 
@@ -21,13 +22,22 @@ function SendMessageCard() {
     <div className="flip-card-wrapper">
       <div className="flip-card-content">
         <div className="send-message-content">
-          <form onSubmit={handleSubmit}>
-            <ReturnButton handleClickFunction={handleClickUnFlipContactCard}/>
-            <GeneralFormInput labelName="Full Name"/>
-            <GeneralFormInput labelName="Email"/>
-            <GeneralFormInput labelName="Subject"/>
-            <GeneralTextArea labelName="Write your message here ..."/>
-            <ContactMeButton type={"submit"} buttonName={"SEND MESSAGE"}/>
+          <form  className="snd-msg-form" onSubmit={handleSubmit}>
+            <div className="row-1">
+              <ReturnButton handleClickFunction={handleClickUnFlipContactCard}/>
+            </div>
+            <div className="row-2">
+              <GeneralFormInput labelName="Full Name"/>
+              <GeneralFormInput labelName="Email"/>
+              <GeneralFormInput labelName="Subject"/>
+              <GeneralFormInput labelName="Phone (optional)"/>
+            </div>
+            <div className="row-3">
+              <GeneralTextArea labelName="Write your message here ..."/>
+            </div>
+            <div className="row-4">
+              <ContactMeButton type={"submit"} buttonName={"SEND MESSAGE"}/>
+            </div>
           </form>
         </div>
       </div>
